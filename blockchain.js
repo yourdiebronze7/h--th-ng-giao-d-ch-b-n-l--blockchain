3abbfe622811097b814169d0867cdcbfcf62c677
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-01 08:40:21 -0700

    Fix Ethereum network URL to use the mainnet

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://network.ethereum');
+const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
 
 const contractAddress = '0x...'; // Địa chỉ smart contract
 const contractABI = []; // ABI của smart contract
