aadb719571e60823d1a8700eae7a9e061dedc5dd
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-01 08:43:04 -0700

    Update log message to English.

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server đang chạy trên http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
