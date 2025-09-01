99031b52d31a4a940e5c5d32d6ae22e196020dba
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-01 08:40:09 -0700

    Updated endpoint to include a summary route for product details.

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const productController = require('./productController');
 const app = express();
 
 app.get('/product/:id/origin', productController.getOrigin);
+app.get('/product/:id/summary', productController.getSummary);
 
 module.exports = app;
