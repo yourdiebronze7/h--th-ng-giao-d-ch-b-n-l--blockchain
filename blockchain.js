7b350faf3427fc7946e303ad823b8824a9580361
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-01 08:41:58 -0700

    Update Infura URL to use environment variable for project ID

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+const web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
 
 const contractAddress = '0x...'; // Address of the smart contract
 const contractABI = []; // ABI of the smart contract
